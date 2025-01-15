301576d4845e4534af0851021ca339bb038e731b
black-adm <mattheuscontato17@gmail.com>
2025-01-15 14:00:45 -0300

    feat: added images and home page base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 import { Button } from "@/components/ui/button";
 
+import logoImg from "@/assets/images/logo-black.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center space-y-6 overflow-hidden">
+      <div className="pt-6">
+        <Image 
+          src={logoImg}
+          width={240}
+          height={40}
+          alt="Logo da Botcolmeia"
+        />
+      </div>
+
       <h1 className="text-6xl font-bol">
         Seja bem vindo
       </h1>
@@ -10,7 +22,9 @@ export default function Home() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed molestiae nemo exercitationem iste aliquid tempore amet modi atque. Tempora adipisci magni nam, possimus quisquam suscipit vero atque voluptatibus in fugiat?
       </p>
 
-      <Button>Vamos começar?</Button>
+      <div className="w-full flex justify-center items-center">
+        <Button className="px-8 py-3.5 w-52 text-base">Vamos começar?</Button>
+      </div>
     </div>
   );
 }
