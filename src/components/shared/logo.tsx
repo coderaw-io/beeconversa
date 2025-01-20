db8ee3471a7a1576add0ef5d6f28f5b3f96a8609
black-adm <mattheuscontato17@gmail.com>
2025-01-20 10:58:46 -0300

    fix: dynamic logo by theme

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,20 +1,27 @@
 "use client"
 
-import logoDarkImg from "@/assets/images/logo-dark.png";
-import logoLightImg from "@/assets/images/logo-light.png";
-import Image from "next/image";
+import logoDarkImg from "@/assets/images/logo-dark.png"
+import logoLightImg from "@/assets/images/logo-light.png"
+import Image from "next/image"
 
-import { useTheme } from "next-themes";
+import { useTheme } from "next-themes"
+import { useLayoutEffect, useState } from "react"
 
 export function Logo() {
-  const { theme } = useTheme();
+  const { theme } = useTheme()
+  const [logoSrc, setLogoSrc] = useState(logoDarkImg)
+
+  useLayoutEffect(() => {
+    setLogoSrc(theme === "light" ? logoDarkImg : logoLightImg)
+  }, [theme])
 
   return (
     <Image
-      src={theme === "light" ? logoDarkImg : logoLightImg}
+      src={logoSrc || "/placeholder.svg"}
       width={280}
       height={40}
       alt="Logo da botcolmeia"
     />
   )
 }
+
