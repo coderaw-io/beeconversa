7b9da25cc95e4c5d2a5b68b83f06671099592553
black-adm <mattheuscontato17@gmail.com>
2025-01-21 17:13:41 -0300

    fix: setup tanstack react query

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import { AppSidebar } from "@/components/shared/app-sidebar"
 import { Header } from "@/components/shared/header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { queryClient } from "@/lib/react-query"
+import { QueryClientProvider } from "@tanstack/react-query"
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -10,16 +14,18 @@ export default function DashboardLayout({
   children
 }: DashboardLayoutProps) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Header />
-        <div className="flex flex-1 flex-col gap-6 pt-4 pb-12 px-8">
-          <main className="max-w-9xl w-full overflow-hidden">
-            {children}
-          </main>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <QueryClientProvider client={queryClient}>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          <div className="flex flex-1 flex-col gap-6 pt-4 pb-12 px-8">
+            <main className="max-w-9xl w-full overflow-hidden">
+              {children}
+            </main>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </QueryClientProvider>
   )
 }
