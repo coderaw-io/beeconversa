c40b93ee48bd7c28972cd72eb94bc92993a0366c
black-adm <mattheuscontato17@gmail.com>
2025-01-22 11:53:12 -0300

    fix: adding typing to sidebar links

--- src/components/shared/sidebar/app-sidebar.tsx
+++ src/components/shared/sidebar/app-sidebar.tsx
@@ -12,35 +12,41 @@ import {
 import {
   CloudUploadIcon,
   HomeIcon,
+  LayoutDashboardIcon,
+  LucideProps,
   MessageCircleIcon,
   RocketIcon,
   Settings2Icon,
-  UsersIcon
-} from 'lucide-react';
+  UsersIcon,
+} from "lucide-react";
 
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
-  TooltipTrigger
+  TooltipTrigger,
 } from "../../ui/tooltip";
 
 import { usePathname } from "next/navigation";
-import { Button } from '../../ui/button';
-import { AppIcon } from '../icons/app-icon';
+import { ForwardRefExoticComponent, RefAttributes } from "react";
+import { Button } from "../../ui/button";
+import { AppIcon } from "../icons/app-icon";
 import { IconNavLink } from "./icon-nav-link";
 import { Logout } from "./logout";
 
-const links = [
+interface AppSidebarLinksProps {
+  href: string
+  icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>
+  tooltip: string
+}
+
+const links: AppSidebarLinksProps[] = [
   { href: "/inicio", icon: HomeIcon, tooltip: "Início" },
-  // { href: "/dashboard", icon: LayoutDashboardIcon, tooltip: "Dashboard" },
+  { href: "/dashboard", icon: LayoutDashboardIcon, tooltip: "Dashboard" },
   { href: "/uploads", icon: CloudUploadIcon, tooltip: "Uploads" },
   { href: "/clientes", icon: UsersIcon, tooltip: "Clientes" },
   { href: "/campanhas", icon: RocketIcon, tooltip: "Campanhas" },
   { href: "/whatsapp", icon: MessageCircleIcon, tooltip: "Whatsapp" },
-  // { href: "/emails", icon: MailsIcon, tooltip: "E-mails" },
-  // { href: "/mensagens", icon: MessagesSquareIcon, tooltip: "SMS" },
-  // { href: "/metricas", icon: ChartNoAxesColumnIcon, tooltip: "Métricas" },
 ];
 
 export function AppSidebar() {
@@ -52,14 +58,14 @@ export function AppSidebar() {
         className="sticky top-0 shrink-0 min-h-screen bg-background w-[70px] border-r border-border"
         collapsible="none"
       >
-        <div className='h-full'>
+        <div className="h-full">
           <SidebarHeader className="h-[70px] border-b border-border">
             <div className="flex h-[70px] w-full items-center justify-center">
               <AppIcon />
             </div>
           </SidebarHeader>
 
-          <SidebarContent className='h-full py-6'>
+          <SidebarContent className="h-full py-6">
             <nav className="flex flex-col gap-6 p-4">
               {links.map(({ href, icon, tooltip }) => (
                 <IconNavLink
@@ -73,44 +79,26 @@ export function AppSidebar() {
             </nav>
           </SidebarContent>
 
-          <SidebarFooter className='h-full mt-auto border-t border-border py-6 px-4 gap-6'>
-            {/* <Link
-              href="/suporte"
-              className="flex items-center justify-center"
-            >
+          <SidebarFooter className="h-full mt-auto border-t border-border py-6 px-4 gap-6">
+            <Link href="/minha-conta" className="flex items-center justify-center">
               <Tooltip>
-                <TooltipTrigger>
+                <TooltipTrigger asChild>
                   <Button
                     type="button"
                     size="icon"
                     variant="outline"
-                    className={pathname === "/suporte" ? "bg-primary dark:bg-yellow-400 border-none" : ""}
-                  >
-                    <LifeBuoyIcon
-                      className={`size-5 ${pathname === "/suporte" ? "text-foreground dark:text-background" : ""}`}
-                    />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent side="right">
-                  <p>Suporte</p>
-                </TooltipContent>
-              </Tooltip>
-            </Link> */}
-
-            <Link
-              href="/minha-conta"
-              className="flex items-center justify-center"
-            >
-              <Tooltip>
-                <TooltipTrigger>
-                  <Button
-                    type="button"
-                    size="icon"
-                    variant="outline"
-                    className={pathname === "/minha-conta" ? "bg-primary dark:bg-yellow-400 border-none" : ""}
+                    className={
+                      pathname === "/minha-conta"
+                        ? "bg-primary dark:bg-yellow-400 border-none"
+                        : ""
+                    }
                   >
                     <Settings2Icon
-                      className={`size-5 ${pathname === "/minha-conta" ? "text-foreground dark:text-background" : ""}`}
+                      className={`size-5 ${
+                        pathname === "/minha-conta"
+                          ? "text-foreground dark:text-background"
+                          : ""
+                      }`}
                     />
                   </Button>
                 </TooltipTrigger>
