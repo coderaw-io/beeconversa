304fd0f53fd38fdf55e4994a680773d0c73c2b35
black-adm <mattheuscontato17@gmail.com>
2025-01-22 15:51:18 -0300

    fix: new filter layout in upload page

--- src/app/(app)/uploads/_components/upload-content.tsx
+++ src/app/(app)/uploads/_components/upload-content.tsx
@@ -2,13 +2,14 @@
 
 import {
   ArrowDownUpIcon,
-  FileDownIcon
+  FileDownIcon,
+  SearchIcon
 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useUploadContext } from "@/hooks/use-upload";
 import { useState } from "react";
-import { UploadsFiltersSection } from "./filters-section";
 import { UploadsTable } from "./upload-table";
 
 export function UploadsContent() {
@@ -18,9 +19,7 @@ export function UploadsContent() {
 
   return (
     <>
-      <div className="space-y-6">
-        <UploadsFiltersSection />
-
+      <div className="pt-6 space-y-6">
         <div className="flex items-center gap-4">
           <Button type="button" variant="default">
             Ver Todos
@@ -30,6 +29,16 @@ export function UploadsContent() {
             <ArrowDownUpIcon className="size-4" />
             Mais antigos
           </Button>
+
+          <div className="relative max-w-md w-full">
+            <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground size-4" />
+
+            <Input
+              className="w-full pl-10 h-11"
+              placeholder="Informe o arquivo que deseja filtrar"
+              maxLength={60}
+            />
+          </div>
 
           <div className="w-full flex justify-end items-center">
             <Button
